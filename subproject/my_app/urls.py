2d579ef97a50b7c9e112879469f3b1d4dc7d14bf
ibnbayo <abdullahifahm@gmail.com>
2022-09-20 15:58:23 +0100

    Add new pages

--- subproject/my_app/urls.py
+++ subproject/my_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.get_home_page, name = 'home'),
     path('about/', views.get_about_page, name = 'about'),
     path('signup/', views.signup, name = 'signup'),
+    path('sides/', views.sides, name = 'sides'),
 
 
 ]
